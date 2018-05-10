d118c81b7da55a969e7e39d0479da77de4d70625
corytveten <corytveten@gmail.com>
2018-05-10 13:28:54 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -14,6 +14,9 @@ class School
   def add_student(name, grade)
     @student_name = name
     @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
     @roster[grade] << name
   end
 
